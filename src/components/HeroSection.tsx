ab476878919b4f0cf1c5dd0efe2308fe0e9f458d
Faiza <faiza@Andreys-MacBook-Pro-2.local>
2026-05-24 15:37:18 -0400

    fix: update sticker paths in hero section to match new directory structure

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -23,10 +23,10 @@ export default function HeroSection() {
   }, [cursorX, cursorY]);
 
   const floatingImages = [
-    { src: '/sticker_0.png', className: 'w-24 md:w-32 rotate-[-8deg] top-[5%] left-[8%]' },
-    { src: '/sticker_2.png', className: 'w-32 md:w-48 rotate-[12deg] top-[35%] right-[5%]' },
-    { src: '/sticker_5.png', className: 'w-24 md:w-36 rotate-[-15deg] bottom-[25%] left-[30%]' },
-    { src: '/sticker_8.png', className: 'w-28 md:w-40 rotate-[5deg] bottom-[10%] right-[15%]' }
+    { src: '/stickers/sticker_0.png', className: 'w-24 md:w-32 rotate-[-8deg] top-[5%] left-[8%]' },
+    { src: '/stickers/sticker_2.png', className: 'w-32 md:w-48 rotate-[12deg] top-[35%] right-[5%]' },
+    { src: '/stickers/sticker_5.png', className: 'w-24 md:w-36 rotate-[-15deg] bottom-[25%] left-[30%]' },
+    { src: '/stickers/sticker_8.png', className: 'w-28 md:w-40 rotate-[5deg] bottom-[10%] right-[15%]' }
   ];
 
   const menuLinks = [
