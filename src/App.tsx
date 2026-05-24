a771d55a66616c1c3b8d275a1c33942526e49ab5
Faiza <faiza@Andreys-MacBook-Pro-2.local>
2026-05-24 15:29:11 -0400

    fix: final import cleanup in hero section

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,10 @@ import Home from './pages/Home';
 import CaseStudy from './pages/CaseStudy';
 import Resume from './pages/Resume';
 import BackToTop from './components/BackToTop';
-import Header from './components/Header';
 
 function App() {
   return (
     <>
-      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/case-study/:id" element={<CaseStudy />} />
